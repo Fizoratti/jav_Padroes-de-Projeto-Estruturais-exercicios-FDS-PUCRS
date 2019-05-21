10ac47c665b706389849cfdc69c7a975feb1fd02
Fizoratti <robson.felipe@acad.pucrs.br>
2019-05-21 20:40:26 -0300

    Add enunciado in the Main class

--- src/App.java
+++ src/App.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
 
+/*
+O padrão Composite serve para implementar uma árvore de itens e tratar todos os nós, folhas ou não,
+de maneira uniforme. Implemente classes que representem um sistema de arquivos, com pastas e arquivos. 
+Pastas possuem nome e diversos arquivos e subpastas. Arquivos possuem nome e tamanho em KB. Seu programa 
+deve navegar pela árvore e imprimir seus itens e tamanhos.
+*/
+
 public class App {
     public static void main(String args[]) {
 
